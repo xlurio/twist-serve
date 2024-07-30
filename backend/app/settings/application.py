4fc9b5d569fe768de14b12274513aab496f8e7d1
Lucas Calegario <lucas.c.calegario@outlook.com>
2024-07-30 14:18:22 -0300

    test: checking env vars

--- backend/app/settings/application.py
+++ backend/app/settings/application.py
@@ -7,7 +7,9 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
+print(env.scheme)
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+print(env.scheme)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY", str)
